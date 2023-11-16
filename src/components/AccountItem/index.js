fd59c6cca8c049fdfd017d2eab7299a034a9e053
NguyenThuong02 <nguyenthanhthuong02@gmail.com>
2023-11-16 19:03:53 +0700

    End-tiktokUI

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,20 +1,21 @@
 import classNames from 'classnames/bind';
 import styles from './AcountItem.module.scss';
+import Image from '~/components/Image';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
-import Img from '../../img/2.jpeg'; // Không nên dùng như này nên tạo 1 file riêng để import
+// import Img from '../../img/2.jpeg'; // Không nên dùng như này nên tạo 1 file riêng để import
 
 const cx = classNames.bind(styles);
 
 function AccountItem() {
     return (
         <div className={cx('wrapper')}>
-            {/* <img
+            <Image
                 className={cx('avatar')}
                 src="https://p16-sign-sg.tiktokcdn.com/aweme/100x100/tos-alisg-avt-0068/7261e2353d2610d05eba8b89601b7e62.jpeg?x-expires=1699902000&x-signature=sH8pDv1zLyeEcYLWjMQMqmMG64c%3D"
                 alt="Hoaa"
-            /> */}
-            <img className={cx('avatar')} src={Img} alt="Hoaa" />
+            />
+            {/* <img className={cx('avatar')} src={Img} alt="Hoaa" /> */}
             <div className={cx('info')}>
                 <h4 className={cx('name')}>
                     <span>Nguyen Van A</span>
